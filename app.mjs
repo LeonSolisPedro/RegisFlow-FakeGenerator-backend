ce8483312c002cbcf6d82a2daf6860d2589c400a
Pedro León <pedro@wintercr.com>
2024-09-23 22:38:03 -0500

    Finishing with the proyect

--- app.mjs
+++ app.mjs
@@ -3,86 +3,44 @@ import cors from "cors"
 import seedrandom from "seedrandom";
 import { body, query } from "express-validator"
 import validation from "./validation.mjs"
-import { readRandomLine, scrambleWord } from "./util.mjs";
+import { generateTable } from "./util.mjs";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.post("/api/generatedata", [
   query("seednumber").notEmpty().isNumeric(),
   query("totalperpage").default(20).isNumeric().toInt(),
-  query("probability").notEmpty().isNumeric().isInt({ min: 0, max: 10 }).toInt(),
-  query("region").notEmpty().isIn(["MXN"])
+  query("probability").notEmpty().isNumeric().isInt({ min: 0, max: 1000 }).toInt(),
+  query("region").notEmpty().isIn(["MXN", "USA", "ITA"])
 ], validation, async (req, res) => {
   const { seednumber, totalperpage, probability, region } = req.query
   const method = seedrandom(seednumber, { state: true })
-  const table = []
-  for (let i of Array(totalperpage).keys()) {
-    //idk what Am I doing
-    const name = await readRandomLine(`dataset/${region}/names.csv`, method)
-    const lastname = await readRandomLine(`dataset/${region}/lastnames.csv`, method)
-    const city = await readRandomLine(`dataset/${region}/cities.csv`, method)
-    const street = await readRandomLine(`dataset/${region}/streets.csv`, method)
-
-    //actually beign used
-    let fullname = `${name} ${lastname}`
-    let address = `${city} ${street}`
-    let phone = await readRandomLine(`dataset/${region}/phones.csv`, method)
-
-    //Applying errors
-    fullname = scrambleWord(fullname, probability,seedrandom(fullname))
-    address = scrambleWord(address, probability,seedrandom(address))
-    phone = scrambleWord(phone, probability,seedrandom(phone))
-
-    table.push([1, 2, fullname, address, phone])
-  }
+  const table = await generateTable(totalperpage, region, probability, method)
   const response = {
     table,
     seeddata: method.state()
   }
   return res.status(200).json(response)
 })
-
-
 
 app.post("/api/continue", [
   body("seeddata").notEmpty(),
   query("totalperpage").default(10).isNumeric().toInt(),
-  query("probability").notEmpty().isNumeric().isInt({ min: 0, max: 10 }).toInt(),
-  query("region").notEmpty().isIn(["MXN"]),
+  query("probability").notEmpty().isNumeric().isInt({ min: 0, max: 1000 }).toInt(),
+  query("region").notEmpty().isIn(["MXN", "USA", "ITA"])
 ], validation, async (req, res) => {
   const { seeddata } = req.body
   const { totalperpage, probability, region } = req.query
   const method = seedrandom("", { state: seeddata })
-  const table = []
-  for (let i of Array(totalperpage).keys()) {
-    //idk what Am I doing
-    const name = await readRandomLine(`dataset/${region}/names.csv`, method)
-    const lastname = await readRandomLine(`dataset/${region}/lastnames.csv`, method)
-    const city = await readRandomLine(`dataset/${region}/cities.csv`, method)
-    const street = await readRandomLine(`dataset/${region}/streets.csv`, method)
-
-    //actually beign used
-    let fullname = `${name} ${lastname}`
-    let address = `${city} ${street}`
-    let phone = await readRandomLine(`dataset/${region}/phones.csv`, method)
-
-    //Applying errors
-    fullname = scrambleWord(fullname, probability,seedrandom(fullname))
-    address = scrambleWord(address, probability,seedrandom(address))
-    phone = scrambleWord(phone, probability,seedrandom(phone))
-
-    table.push([1, 2, fullname, address, phone])
-  }
+  const table = await generateTable(totalperpage, region, probability, method)
   const response = {
     table,
     seeddata: method.state()
   }
   return res.status(200).json(response)
 })
-
 
 const port = 8080;
 app.listen(port, () => {
--- util.mjs
+++ util.mjs
@@ -1,7 +1,12 @@
 import fs from "fs/promises"
+import seedrandom from "seedrandom";
+import { v4 as uuidv4 } from "uuid"
 const DELIMITER = '\n'
 const READ_BUFFER_SIZE = 1000 // Must be greater than the record size
 
+/*
+ * https://stackoverflow.com/a/76759865/20961125
+ */
 export async function readRandomLine(filepath, seedrandom) {
   const stats = await fs.stat(filepath)
   const handle = await fs.open(filepath, 'r')
@@ -16,10 +21,60 @@ export async function readRandomLine(filepath, seedrandom) {
 }
 
 
-export function scrambleWord(word, probability,methodScram) {
-  return word.split("").map(word => {
+export function scrambleWord(string, probability, methodScram) {
+  return string.split("").map((word, i, arr) => {
     if (word === " ") return word
-    if ((methodScram() * 10) < probability) return "X"
-    return word
-  }).join("")
+    if ((methodScram() * 1000) < probability) {
+      const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+      const randomItem = Math.floor(methodScram() * 3);
+      if (randomItem === 0) return ""
+      if (randomItem === 1) {
+        const letter = Math.floor(methodScram() * alphabet.length);
+        return alphabet[letter];
+      }
+      if (randomItem === 2) {
+        // idk what I'm doing
+        if (i > 0) { 
+          let temp = arr[i];
+          arr[i] = arr[i - 1];
+          arr[i - 1] = temp;
+          return arr[i];
+        } else if (i < arr.length - 1) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          return arr[i];
+        }
+      }
+    }
+    return word;
+  }).join("");
+}
+
+
+
+export function generateUuid(method) {
+  return uuidv4({
+    random: Array.from({ length: 16 }, () => Math.floor(method() * 256))
+  });
+}
+
+
+export async function generateTable(totalperpage, region, probability, method) {
+  const table = []
+  for (let i of Array(totalperpage).keys()) {
+    //Reading
+    const name = await readRandomLine(`dataset/${region}/names.txt`, method)
+    const lastname = await readRandomLine(`dataset/${region}/lastnames.txt`, method)
+    let fullname = `${name} ${lastname}`
+    let address = await readRandomLine(`dataset/${region}/addresses.txt`, method)
+    let phone = await readRandomLine(`dataset/${region}/phones.txt`, method)
+    const uuid = generateUuid(seedrandom(fullname + address + phone))
+    //Applying errors
+    fullname = scrambleWord(fullname, probability, seedrandom(fullname))
+    address = scrambleWord(address, probability, seedrandom(address))
+    phone = scrambleWord(phone, probability, seedrandom(phone))
+    table.push([uuid, fullname, address, phone])
+  }
+  return table;
 }
